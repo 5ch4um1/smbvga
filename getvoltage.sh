fb8855080b613c9f8698d8acff02c006bc897ca8
5ch4um1 <36307725+5ch4um1@users.noreply.github.com>
2020-02-10 00:07:05 +0000

    Update getvoltage.sh

--- getvoltage.sh
+++ getvoltage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-sudo i2cget -y 1 0x0b 0x09 w | awk ' {print substr($1,3); }' | awk ' {$1=strtonum("0x" $1)}  gsub("^.{1}","&.") {print "Voltage is at: " $1 " V"} '
+sudo i2cget -y 1 0x0b 0x09 w | awk ' {print substr($1,3); }' | awk ' {$1=strtonum("0x" $1)}  gsub("^.{1}","&.") {print "Voltage is: " $1 " V"} '
