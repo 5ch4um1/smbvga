3b255471cfe63118de48313aa2bc4a1d1cb037ed
5ch4um1 <36307725+5ch4um1@users.noreply.github.com>
2020-02-11 15:05:01 +0000

    Update dumpreg.sh

--- dumpreg.sh
+++ dumpreg.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #
-# this aparently dumps the content of the registers(?). the longest on my chip seems to be 33 bytes, however, i set it to 64, doesn't seem to hurt.  
-#use ./dump.sh &> dump.txt to dump everything to a text file. waiting one second might be a bit generous, you might lower that a bit?
+# this aparently dumps the content of the registers(?), from 0x00 to 0x75 
+# the longest on my chip seems to be 33 bytes, however, i set it to 64, doesn't seem to hurt.  
+# use ./dump.sh &> dump.txt to dump everything to a text file. waiting one second might be a bit generous, you might lower that a bit?
 #
 
 max=75
