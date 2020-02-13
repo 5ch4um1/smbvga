9aa71b1e8ace7543dc7bb490473b32b6cf944e56
5ch4um1 <36307725+5ch4um1@users.noreply.github.com>
2020-02-13 14:17:57 +0000

    Update dumpreg.sh

--- dumpreg.sh
+++ dumpreg.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #
-# this aparently dumps the content of the registers(?), from 0x01 to 0x75 (?)
-# the longest on my chip seems to be 33 bytes, however, i set it to 64, doesn't seem to hurt.  
+# this aparently dumps the content of the registers(?), from 0x00 to 0x76 
+# the longest on my chip seems to be 34 bytes. 
 # use ./dump.sh &> dump.txt to dump everything to a text file. waiting one second might be a bit generous, you might lower that a bit?
 #
 
 max=118
-for ((i=1;i<=max;i++))
+for ((i=0;i<=max;i++))
 do
   i=$(printf "%#.2x\n" $i)
 
 
-sudo i2ctransfer -y 1 w1@0x0b $i r64
+sudo i2ctransfer -y 1 w1@0x0b $i r34
 sleep 1
 done
