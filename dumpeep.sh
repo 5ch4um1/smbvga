e243c916a2b84850510cce102ff64fda8767d6d4
5ch4um1 <36307725+5ch4um1@users.noreply.github.com>
2020-12-24 22:24:24 +0000

    Create dumpeep.sh
    this should dump the content of the eeprom

---
+++ dumpeep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+max=18416
+for ((i=16384;i<=max;))
+
+do
+  y=$(printf "%#.4x\n" $i)
+let "i+=16"
+
+
+sudo i2cset -y 1 0x0b 0x09 $y w
+sleep 0.01
+r=$(sudo i2ctransfer -y 1 w1@0x0b 0x0c r17)
+echo $r | cut -c6-
+sleep 0.01
+done
