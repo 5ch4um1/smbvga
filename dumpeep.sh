807d128ac62199e483c5177a1e1d6489e3bff6ff
5ch4um1 <36307725+5ch4um1@users.noreply.github.com>
2020-12-25 14:19:01 +0000

    Update dumpeep.sh
    one day you feel smart, the other day it takes you ages to realize that 20 means 0x20, aka 32...that's life... :o

--- dumpeep.sh
+++ dumpeep.sh
@@ -5,12 +5,12 @@ for ((i=16384;i<=max;))
 
 do
   y=$(printf "%#.4x\n" $i)
-let "i+=16"
+let "i+=32"
 
 
 sudo i2cset -y 1 0x0b 0x09 $y w
 sleep 0.01
-r=$(sudo i2ctransfer -y 1 w1@0x0b 0x0c r17)
+r=$(sudo i2ctransfer -y 1 w1@0x0b 0x0c r33)
 echo $r | cut -c6-
 sleep 0.01
 done
